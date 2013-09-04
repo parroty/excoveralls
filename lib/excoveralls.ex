3ebb6e060030ff68f714d2db0fc30a7dc1bf5dc8
parroty <parroty00@gmail.com>
2013-09-05 00:52:50 +0900

    fix naming reference

--- lib/excoveralls.ex
+++ lib/excoveralls.ex
@@ -3,6 +3,11 @@ defmodule ExCoveralls do
   Provides the entry point for coverage calculation and output.
   This module method is called by Mix.Tasks.Test
   """
+  alias ExCoveralls.Stats
+  alias ExCoveralls.Cover
+  alias ExCoveralls.Generator
+  alias ExCoveralls.Poster
+
   def start(compile_path, option) do
     compile(compile_path)
 
@@ -13,12 +18,12 @@ defmodule ExCoveralls do
   end
 
   def compile(compile_path) do
-    ExCoveralls.Cover.compile(compile_path)
+    Cover.compile(compile_path)
   end
 
   def calculate(option) do
     Stats.calculate(Cover.modules)
-      |> ExCoveralls.Generator.execute(option[:type])
-      |> ExCoveralls.Poster.execute
+      |> Generator.execute(option[:type])
+      |> Poster.execute
   end
 end
