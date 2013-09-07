d5cc810bd5440292d5f75a261e7dd3046af31728
parroty <parroty00@gmail.com>
2013-09-07 13:48:58 +0900

    fix curl parameter

--- lib/excoveralls/poster.ex
+++ lib/excoveralls/poster.ex
@@ -3,7 +3,7 @@ defmodule ExCoveralls.Poster do
   Post JSON to coveralls server
   """
   @file_name "excoveralls.post.json"
-  @post_cmd  "curl \"https://coveralls.io/api/v1/jobs\" -F json_file=#{@file_name}"
+  @post_cmd  "curl \"https://coveralls.io/api/v1/jobs\" -F json_file=@#{@file_name}"
 
   @doc """
   Create a temporarily json file and post it to server using curl command.
@@ -11,7 +11,7 @@ defmodule ExCoveralls.Poster do
   """
   def execute(json) do
     File.write!(@file_name, json)
-    IO.inspect System.cmd(@post_cmd)
+    IO.puts System.cmd(@post_cmd)
     File.rm!(@file_name)
   end
 end
