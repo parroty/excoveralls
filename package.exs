69fdce62955a69e39178830c0cfe105d8388b20f
parroty <parroty00@gmail.com>
2013-09-08 02:56:06 +0900

    add package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "excoveralls", description: "coveralls.io for Elixir",
+                 version: "0.0.1", keywords: ["cover","coveralls"],
+                 maintainers: [[name: "parroty", email: "parroty00@gmail.com"]],
+                 repositories: [[github: "parroty/excoveralls"]])
