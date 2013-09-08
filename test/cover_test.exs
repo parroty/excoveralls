68de267fce7835ef92559f6fa322cc539d56a753
parroty <parroty00@gmail.com>
2013-09-08 16:07:44 +0900

    add tests

--- test/cover_test.exs
+++ test/cover_test.exs
@@ -2,6 +2,10 @@ defmodule CoverTest do
   use ExUnit.Case
   alias ExCoveralls.Cover
 
+  test "compile returns list" do
+    assert(Cover.compile(".") |> is_list)
+  end
+
   test "module path returns relative path" do
     assert(Cover.module_path(ExCoveralls) == "lib/excoveralls.ex")
   end
