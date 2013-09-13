b7ae284d6a392dd1c1a8160fd850a1b5fb7d693c
parroty <parroty00@gmail.com>
2013-09-13 21:53:59 +0900

    make version 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExCoveralls.Mixfile do
 
   def project do
     [ app: :excoveralls,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 0.10.3-dev",
       deps: deps,
     ]
