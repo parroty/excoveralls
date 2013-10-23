cc4a3cb613fc5cfca2eab2ca0f1e6f5891ca6840
parroty <parroty00@gmail.com>
2013-10-24 00:08:56 +0900

    re-add tests for tasks

--- test/mix/tasks_test.exs
+++ test/mix/tasks_test.exs
@@ -1,27 +1,41 @@
 Code.require_file "../test_helper.exs", __DIR__
 
-# defmodule Mix.Tasks.CoverallsTest do
-#   use ExUnit.Case
-#   import Mock
+defmodule Mix.Tasks.CoverallsTest do
+  use ExUnit.Case
+  import Mock
 
-#   test_with_mock "local", Mix.Task, [run: fn(_, _) -> nil end] do
-#     assert(Mix.Tasks.Coveralls.run([]) == ExCoveralls.Local.Mixfile)
-#     assert(called Mix.Task.run("test", ["--cover"]))
-#   end
+  # backup the original config
+  setup do
+    ExCoveralls.ConfServer.start
+    {:ok, from_setup: ExCoveralls.ConfServer.get}
+  end
 
-#   test_with_mock "detail", Mix.Task, [run: fn(_, _) -> nil end] do
-#     assert(Mix.Tasks.Coveralls.Detail.run([]) == ExCoveralls.Detail.Mixfile)
-#     assert(called Mix.Task.run("test", ["--cover"]))
-#   end
+  # restore the original config
+  teardown meta do
+    ExCoveralls.ConfServer.set(meta[:from_setup])
+  end
 
-#   test_with_mock "travis", Mix.Task, [run: fn(_, _) -> nil end] do
-#     assert(Mix.Tasks.Coveralls.Travis.run([]) == ExCoveralls.Travis.Mixfile)
-#     assert(called Mix.Task.run("test", ["--cover"]))
-#   end
+  test_with_mock "local", Mix.Task, [run: fn(_, _) -> nil end] do
+    Mix.Tasks.Coveralls.run([])
+    assert(called Mix.Task.run("test", ["--cover"]))
+    assert(ExCoveralls.ConfServer.get == [type: "local"])
+  end
 
-#   test_with_mock "post", Mix.Task, [run: fn(_, _) -> nil end] do
-#     assert(Mix.Tasks.Coveralls.Post.run([]) == ExCoveralls.Post.Mixfile)
-#     assert(called Mix.Task.run("test", ["--cover"]))
-#   end
+  test_with_mock "detail", Mix.Task, [run: fn(_, _) -> nil end] do
+    Mix.Tasks.Coveralls.Detail.run([])
+    assert(called Mix.Task.run("test", ["--cover"]))
+    assert(ExCoveralls.ConfServer.get == [type: "local", detail: true])
+  end
 
-# end
+  test_with_mock "travis", Mix.Task, [run: fn(_, _) -> nil end] do
+    Mix.Tasks.Coveralls.Travis.run([])
+    assert(called Mix.Task.run("test", ["--cover"]))
+    assert(ExCoveralls.ConfServer.get == [type: "travis"])
+  end
+
+  test_with_mock "post", Mix.Task, [run: fn(_, _) -> nil end] do
+    Mix.Tasks.Coveralls.Post.run([])
+    assert(called Mix.Task.run("test", ["--cover"]))
+    assert(ExCoveralls.ConfServer.get == [type: "post"])
+  end
+end
