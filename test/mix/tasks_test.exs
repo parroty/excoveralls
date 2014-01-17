2bff269a22e91eac60ef3f4d0d043d0907270ae2
parroty <parroty00@gmail.com>
2014-01-18 07:35:22 +0900

    Set async: false for mix task tests

--- test/mix/tasks_test.exs
+++ test/mix/tasks_test.exs
@@ -1,7 +1,7 @@
 Code.require_file "../test_helper.exs", __DIR__
 
 defmodule Mix.Tasks.CoverallsTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   import Mock
   import ExUnit.CaptureIO
 
