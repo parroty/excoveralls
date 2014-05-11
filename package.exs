e5759d75dc6f38b7bd7d0c440a5d74a7c1c6c0e3
parroty <parroty00@gmail.com>
2014-05-11 22:46:38 +0900

    Remove files

--- package.exs
+++
@@ -1,4 +0,0 @@
-Expm.Package.new(name: "excoveralls", description: "coveralls.io for Elixir",
-                 version: "0.2.0", keywords: ["cover","coveralls"],
-                 maintainers: [[name: "parroty", email: "parroty00@gmail.com"]],
-                 repositories: [[github: "parroty/excoveralls"]])
