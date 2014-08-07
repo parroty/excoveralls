9715d3c6942fd2287b8b2547c444ec8c5559e390
parroty <parroty00@gmail.com>
2014-08-07 17:39:07 +0900

    Now requires ~> v0.14.3 for System.cmd

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCoveralls.Mixfile do
   def project do
     [ app: :excoveralls,
       version: "0.3.2",
-      elixir: "~> 0.14.0 or ~> 0.15.0",
+      elixir: "~> 0.14.3 or ~> 0.15.0",
       deps: deps,
       description: description,
       package: package,
