12c9d2a4270d8f387e3ed3ac90ac98302a380256
parroty <parroty00@gmail.com>
2015-01-11 21:03:48 +0900

    Remove conflicting test

--- test/cover_test.exs
+++ test/cover_test.exs
@@ -2,10 +2,6 @@ defmodule CoverTest do
   use ExUnit.Case
   alias ExCoveralls.Cover
 
-  test "compile returns list" do
-    assert(Cover.compile(".") |> is_list)
-  end
-
   test "module path returns relative path" do
     assert(Cover.module_path(ExCoveralls) == "lib/excoveralls.ex")
   end
