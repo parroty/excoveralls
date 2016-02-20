716c99c0844558a19feece890af1d2fac69f8953
parroty <parroty00@gmail.com>
2016-02-20 21:25:58 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExCoveralls.Mixfile do
 
   def project do
     [ app: :excoveralls,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.0",
       deps: deps,
       description: description,
