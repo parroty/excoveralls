41bb0b5a43ac06b0c51d858fc23b083d0a232fca
Mijail Rondon <mijail@riseup.net>
2019-11-22 03:58:01 -0500

    undo changes

--- test/circle_test.exs
+++ test/circle_test.exs
@@ -3,17 +3,19 @@ defmodule ExCoveralls.CircleTest do
   import Mock
   alias ExCoveralls.Circle
 
-  @content "defmodule Test do\n  def test do\n  end\nend\n"
-  @counts [0, 1, nil, nil]
-  @source_info [%{name: "test/fixtures/test.ex", source: @content, coverage: @counts}]
+  @content     "defmodule Test do\n  def test do\n  end\nend\n"
+  @counts      [0, 1, nil, nil]
+  @source_info [%{name: "test/fixtures/test.ex",
+                  source: @content,
+                  coverage: @counts
+               }]
 
   setup do
     # Capture existing values
-    orig_vars =
-      ~w(CI_PULL_REQUEST CIRCLE_USERNAME CIRCLE_SHA1 CIRCLE_BRANCH CIRCLE_BUILD_NUM COVERALLS_REPO_TOKEN)
-      |> Enum.map(fn var -> {var, System.get_env(var)} end)
+    orig_vars = ~w(CI_PULL_REQUEST CIRCLE_USERNAME CIRCLE_SHA1 CIRCLE_BRANCH CIRCLE_BUILD_NUM COVERALLS_REPO_TOKEN)
+    |> Enum.map(fn var -> {var, System.get_env(var)} end)
 
-    on_exit(fn ->
+    on_exit fn ->
       # Reset env vars
       for {k, v} <- orig_vars do
         if v != nil do
@@ -22,14 +24,14 @@ defmodule ExCoveralls.CircleTest do
           System.delete_env(k)
         end
       end
-    end)
+    end
 
     # No additional context
     {:ok, []}
   end
 
-  test_with_mock "execute", ExCoveralls.Poster, execute: fn _ -> "result" end do
-    assert(Circle.execute(@source_info, []) == "result")
+  test_with_mock "execute", ExCoveralls.Poster, [execute: fn(_) -> "result" end] do
+    assert(Circle.execute(@source_info,[]) == "result")
   end
 
   test "generate json for circle" do
@@ -42,8 +44,8 @@ defmodule ExCoveralls.CircleTest do
   end
 
   test "submits as `circle-ci` by default" do
-    parsed = Circle.generate_json(@source_info) |> Jason.decode!()
-    assert(%{"service_name" => "circle-ci"} = parsed)
+    parsed = Circle.generate_json(@source_info) |> Jason.decode!
+    assert(%{ "service_name" => "circle-ci" } = parsed)
   end
 
   test "generate from env vars" do
@@ -55,15 +57,17 @@ defmodule ExCoveralls.CircleTest do
     System.put_env("COVERALLS_REPO_TOKEN", "token")
 
     {:ok, payload} = Jason.decode(Circle.generate_json(@source_info))
-
-    %{"git" => %{"branch" => branch, "head" => %{"committer_name" => committer_name, "id" => id}}} =
-      payload
+    %{"git" =>
+      %{"branch" => branch,
+        "head" => %{"committer_name" => committer_name,
+                  "id" => id}}} = payload
 
     assert(payload["service_pull_request"] == "39")
     assert(branch == "branch")
-    assert(id == "sha1-PR-206")
+    assert(id == "sha1")
     assert(committer_name == "username")
     assert(payload["service_number"] == "0")
     assert(payload["repo_token"] == "token")
   end
+
 end
