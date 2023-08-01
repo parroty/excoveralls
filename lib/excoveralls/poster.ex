abd7dbffc8da00714a70174cc357f75d35ebda76
Andrea Leopardi <an.leopardi@gmail.com>
2023-08-01 11:24:12 +0200

    FIXUP

--- lib/excoveralls/poster.ex
+++ lib/excoveralls/poster.ex
@@ -92,28 +92,46 @@ defmodule ExCoveralls.Poster do
     end
   end
 
-  defp cacert_option do
-    if Code.ensure_loaded?(CAStore) do
-      [cacertfile: String.to_charlist(CAStore.file_path())]
-    else
-      case :public_key.cacerts_load() do
-        :ok ->
-          [cacerts: :public_key.cacerts_get()]
+  if function_exported?(:public_key, :cacerts_load, 0) do
+    defp cacert_option do
+      if Code.ensure_loaded?(CAStore) do
+        [cacertfile: String.to_charlist(CAStore.file_path())]
+      else
+        case :public_key.cacerts_load() do
+          :ok ->
+            [cacerts: :public_key.cacerts_get()]
 
-        {:error, reason} ->
-          raise ExCoveralls.ReportUploadError,
-            message: """
-            Failed to load OS certificates. We tried to use OS certificates because we
-            couldn't find the :castore library. If you want to use :castore, please add
+          {:error, reason} ->
+            raise ExCoveralls.ReportUploadError,
+              message: """
+              Failed to load OS certificates. We tried to use OS certificates because we
+              couldn't find the :castore library. If you want to use :castore, please add
 
-              {:castore, "~> 1.0"}
+                {:castore, "~> 1.0"}
 
-            to your dependencies. Otherwise, make sure you can load OS certificates by
-            running :public_key.cacerts_load() and checking the result. The error we
-            got was:
+              to your dependencies. Otherwise, make sure you can load OS certificates by
+              running :public_key.cacerts_load() and checking the result. The error we
+              got was:
 
-              #{inspect(reason)}
-            """
+                #{inspect(reason)}
+              """
+        end
+      end
+    end
+  else
+    defp cacert_option do
+      if Code.ensure_loaded?(CAStore) do
+        [cacertfile: String.to_charlist(CAStore.file_path())]
+      else
+        raise ExCoveralls.ReportUploadError,
+          message: """
+          Failed to use any SSL certificates. We didn't find the :castore library,
+          and we couldn't use OS certificates because that requires OTP 25 or later.
+          If you want to use :castore, please add
+
+            {:castore, "~> 1.0"}
+
+          """
       end
     end
   end
